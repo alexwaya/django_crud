c31fcc77d5e5c53ec4f23f0709f056484578ed40
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-29 19:22:19 +0300

    add create and read view as index

--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Student
+from .forms import CreateStudentForm
 
-# Create your views here.
+
+def CreateStudent(request):
+    if request.method == "POST":
+        form = CreateStudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = CreateStudentForm()
+    else:
+        form = CreateStudentForm()
+
+    students = Student.objects.all().order_by("-id")
+    context = {'form':form, 'students':students}
+    return render(request, 'home.html', context)
