600d36ab17d9afccbdb6fbb35eabc07835766880
Alexander Waya Nguku <alex.waya@gmail.com>
2022-11-29 19:26:07 +0300

    add detail and update view & template

--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Student
 from .forms import CreateStudentForm
 
@@ -15,3 +16,28 @@ def CreateStudent(request):
     students = Student.objects.all().order_by("-id")
     context = {'form':form, 'students':students}
     return render(request, 'home.html', context)
+
+
+def GetStudent(request, **kwargs):
+    reg = kwargs.get('reg')
+    student = get_object_or_404(Student, reg_no=reg)
+
+    if request.method == "POST":
+        form = CreateStudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CreateStudentForm(instance=student)
+
+    context = {'student':student, 'form': form}
+    return render(request, 'std_detail.html', context )
+
+
+
+
+
+
+
+
+
+
